ac6d6627eb2fef25b0a6da088b59e93565084556
Aarón Sánchez Navarro <aaron.sn.1988@gmail.com>
2013-11-04 18:18:18 +0100

    Creado formulario para iniciar sesión.

---
+++ Gebat/GebatWin/Inicio.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GebatWin
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.maskedTextBoxPassword = new System.Windows.Forms.MaskedTextBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuairo: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña: ";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Enabled = false;
+            this.buttonConnect.Location = new System.Drawing.Point(12, 98);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 3;
+            this.buttonConnect.Text = "Conectar";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(85, 25);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUser.TabIndex = 1;
+            this.textBoxUser.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // maskedTextBoxPassword
+            // 
+            this.maskedTextBoxPassword.Location = new System.Drawing.Point(85, 51);
+            this.maskedTextBoxPassword.Name = "maskedTextBoxPassword";
+            this.maskedTextBoxPassword.PasswordChar = '*';
+            this.maskedTextBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBoxPassword.TabIndex = 2;
+            this.maskedTextBoxPassword.TextChanged += new System.EventHandler(this.maskedTextBoxPassword_TextChanged);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(94, 98);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 4;
+            this.buttonExit.Text = "Salir";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 163);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.maskedTextBoxPassword);
+            this.Controls.Add(this.textBoxUser);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Inicio";
+            this.Text = "Gebat-Inicio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxPassword;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Gebat/GebatWin/Inicio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GebatWin
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            enableConnect();   
+        }
+
+        private void enableConnect()
+        {
+            if (textBoxUser.Text != "" && maskedTextBoxPassword.Text != "")
+            {
+                buttonConnect.Enabled = true;
+            }
+        }
+
+        private void maskedTextBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+            enableConnect();
+        }
+    }
+}
