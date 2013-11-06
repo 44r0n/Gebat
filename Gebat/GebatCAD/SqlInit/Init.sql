f80952f533e75355b607e7755e826ca34d99db2f
Aarón Sánchez Navarro <aaron.sn.1988@gmail.com>
2013-11-06 12:23:17 +0100

    Creación del script de pruebas. Modificada la tabla Food, ahora las cantidades están en las tablas EntryFood para la comida entrante, y OutgoingFood para la saliente. Problemas para la creación de la vista que reune el campo calculado de comida entrante - saliente.

--- Gebat/GebatCAD/SqlInit/Init.sql
+++ Gebat/GebatCAD/SqlInit/Init.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS OutgoingFood;
+DROP TABLE IF EXISTS EntryFood;
 DROP TABLE IF EXISTS Food;
 DROP TABLE IF EXISTS Type;
 
@@ -11,7 +13,22 @@ CREATE TABLE IF NOT EXISTS Food
 (
 	Id int Primary Key AUTO_INCREMENT,
 	Name varchar(20) not null,
-	Quantity int not null,
 	QuantityType int,
 	CONSTRAINT fk_Food_Type FOREIGN KEY (QuantityType) REFERENCES Type (Id) ON UPDATE SET NULL ON DELETE SET NULL
 );
+
+CREATE TABLE IF NOT EXISTS EntryFood
+(
+	Id int Primary Key AUTO_INCREMENT,
+	FoodType int,
+	Quantity int,
+	CONSTRAINT fk_EntryFood_Food FOREIGN KEY (FoodType) REFERENCES Food (Id) ON UPDATE SET NULL ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS OutgoingFood
+(
+	Id int Primary Key AUTO_INCREMENT,
+	FoodType int,
+	Quantity int,
+	CONSTRAINT fk_OutgoingFood_Food FOREIGN KEY (FoodType) REFERENCES Food (Id) ON UPDATE SET NULL ON DELETE SET NULL
+)
