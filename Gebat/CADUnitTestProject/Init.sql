00b88bb7ef5380cdd0a26bce6c39f8b22f9acced
Aarón Sánchez Navarro <aaron.sn.1988@gmail.com>
2013-11-08 00:07:26 +0100

    Creado script de creación y pruebas que incluyen personas y tbc.

--- Gebat/CADUnitTestProject/Init.sql
+++ Gebat/CADUnitTestProject/Init.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS TBC;
+DROP TABLE IF EXISTS Personas;
 DROP TABLE IF EXISTS OutgoingFood;
 DROP TABLE IF EXISTS EntryFood;
 DROP TABLE IF EXISTS Food;
@@ -22,6 +24,7 @@ CREATE TABLE IF NOT EXISTS EntryFood
 	Id int Primary Key AUTO_INCREMENT,
 	FoodType int,
 	QuantityIn int,
+	Fecha date,
 	CONSTRAINT fk_EntryFood_Food FOREIGN KEY (FoodType) REFERENCES Food (Id) ON UPDATE SET NULL ON DELETE SET NULL
 );
 
@@ -30,8 +33,29 @@ CREATE TABLE IF NOT EXISTS OutgoingFood
 	Id int Primary Key AUTO_INCREMENT,
 	FoodType int,
 	QuantityOut int,
+	Fecha date,
 	CONSTRAINT fk_OutgoingFood_Food FOREIGN KEY (FoodType) REFERENCES Food (Id) ON UPDATE SET NULL ON DELETE SET NULL
 );
+
+CREATE TABLE IF NOT EXISTS Personas 
+(
+  DNI CHAR(9) Primary Key,
+  Nombre VARCHAR(15) NULL,
+  Apellidos VARCHAR(45) NULL
+);
+
+CREATE TABLE IF NOT EXISTS TBC 
+(
+  DNI CHAR(9) NOT NULL,
+  Ejecutoria VARCHAR(10) NOT NULL,
+  Juzgado VARCHAR(45) NULL,
+  FInicio DATE NULL,
+  FFin DATE NULL,
+  PRIMARY KEY (DNI, Ejecutoria),
+  CONSTRAINT fk_TBC_Personas FOREIGN KEY (DNI) REFERENCES Personas (DNI) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
 
 CREATE OR REPLACE VIEW TotalFood as select (entrada.Quantity - salida.Quantity) as Total, entrada.FoodType FROM entrada inner join salida on (entrada.FoodType = salida.FoodType);
 
