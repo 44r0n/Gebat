77feaa7e03263040186b81530758f9d64daa2387
Aarón Sánchez Navarro <aaron.sn.1988@gmail.com>
2013-11-09 14:40:39 +0100

    Añadido ListaFoodIN.

---
+++ Gebat/WindowComponents/Lists/ListaFoodIN.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using GebatEN.Classes;
+
+namespace GebatWindowComponents.Lists
+{
+    public class ListaFoodIN : ListaGeneral
+    {
+        #region//Protected Methods
+
+        protected override void MostrarElementos()
+        {
+            Items.Clear();
+            int i = 0;
+            foreach (AEN it in colection)
+            {
+                ENFoodIN entrada = (ENFoodIN)it;
+            }
+        }
+
+        #endregion
+
+
+    }
+}
