19989b52135a410ee7f79eaca8a8e33c97bde93c
Aarón Sánchez Navarro <aaron.sn.1988@gmail.com>
2014-05-19 10:27:37 +0200

    Añadida y probada librería de encriptación simétrica.

---
+++ Gebat/Cripto/Util/Cipher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Cripto.Util
+{
+    public class Cipher
+    {
+        public static String Encrypt<T>(string value, string password, string salt)
+            where T : SymmetricAlgorithm, new()
+        {
+            DeriveBytes rgb = new Rfc2898DeriveBytes(password, Encoding.Unicode.GetBytes(salt));
+
+            SymmetricAlgorithm algorithm = new T();
+            byte[] rgbkey = rgb.GetBytes(algorithm.KeySize >> 3);
+            byte[] rgbIV = rgb.GetBytes(algorithm.BlockSize >> 3);
+
+            ICryptoTransform transform = algorithm.CreateEncryptor(rgbkey, rgbIV);
+            using (MemoryStream buffer = new MemoryStream())
+            {
+                using (CryptoStream stream = new CryptoStream(buffer, transform, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter writer = new StreamWriter(stream, Encoding.Unicode))
+                    {
+                        writer.Write(value);
+                    }
+                }
+
+                return Convert.ToBase64String(buffer.ToArray());
+            }
+        }
+
+        public static String Decrypt<T>(String ciphered, string password, string salt)
+            where T : SymmetricAlgorithm, new()
+        {
+            DeriveBytes rgb = new Rfc2898DeriveBytes(password, Encoding.Unicode.GetBytes(salt));
+
+            SymmetricAlgorithm algorithm = new T();
+            byte[] rgbkey = rgb.GetBytes(algorithm.KeySize >> 3);
+            byte[] rgbIV = rgb.GetBytes(algorithm.BlockSize >> 3);
+
+            ICryptoTransform transform = algorithm.CreateDecryptor(rgbkey, rgbIV);
+            using (MemoryStream buffer = new MemoryStream(Convert.FromBase64String(ciphered)))
+            {
+                using (CryptoStream stream = new CryptoStream(buffer, transform, CryptoStreamMode.Read))
+                {
+                    using (StreamReader reader = new StreamReader(stream, Encoding.Unicode))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Gebat/CriptoUnitTest/CriptoTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cripto.Util;
+using LLCryptoLib.Crypto;
+using System.Security.Cryptography;
+
+namespace CriptoUnitTest
+{
+    [TestClass]
+    public class CriptoTest
+    {
+        [TestMethod]
+        public void TestBlowFish()
+        {
+            String tocrypt = "hola mundo";
+            String cifrado = Cipher.Encrypt<BlowfishManaged>(tocrypt, "contra", "saltedea");
+            String descifrado = Cipher.Decrypt<BlowfishManaged>(cifrado, "contra", "saltedea");
+            Assert.AreEqual(tocrypt, descifrado);
+        }
+
+        [TestMethod]
+        public void TestAES()
+        {
+            String tocrypt = "hola mundo";
+            String cifrado = Cipher.Encrypt<AesManaged>(tocrypt, "contra", "saltedea");
+            String descifrado = Cipher.Decrypt<AesManaged>(cifrado, "contra", "saltedea");
+            Assert.AreEqual(tocrypt, descifrado);
+        }
+
+        [TestMethod]
+        public void TestAll()
+        {
+            String tocrypt = "12345678A";
+            String cifrado = Cipher.Encrypt<BlowfishManaged>(tocrypt, "contra", "saltedea");
+            cifrado = Cipher.Encrypt<TripleDESCryptoServiceProvider>(cifrado, "otracon", "otrasalde");
+            cifrado = Cipher.Encrypt<AesManaged>(cifrado, "terceracon", "saldeaes");
+            cifrado = Cipher.Encrypt<RijndaelManaged>(cifrado, "conderijn", "salderij");
+
+            String descifrado = Cipher.Decrypt<RijndaelManaged>(cifrado, "conderijn", "salderij");
+            descifrado = Cipher.Decrypt<AesManaged>(descifrado, "terceracon", "saldeaes");
+            descifrado = Cipher.Decrypt<TripleDESCryptoServiceProvider>(descifrado, "otracon", "otrasalde");
+            descifrado = Cipher.Decrypt<BlowfishManaged>(descifrado, "contra", "saltedea");
+
+            Assert.AreEqual(tocrypt, descifrado);
+        }
+    }
+}
